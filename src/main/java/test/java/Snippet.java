b6fa2f189c2c41fa3c36ccd33422b028f8f85a3d
ChuangRuby <glwl40039@gmail.com>
2020-04-28 17:16:28 +0800

    java爬蟲更新可以抓資訊首頁的資料(標題 日期 超連結網址 小段落)但超連結的分頁內容...Q_Q

--- src/main/java/test/java/Snippet.java
+++ src/main/java/test/java/Snippet.java
@@ -37,7 +37,7 @@ public class Snippet {
 	    // 屏蔽HtmlUnit等系统 log  
 	    java.util.logging.Logger.getLogger("org.apache.http.client").setLevel(Level.OFF);  
 	
-	    String url = "https://www.cardu.com.tw/news/list.php?nt_pk=6";  
+	    String url = "https://www.cardu.com.tw/news/list.php?nt_pk=6";  //要抓取的網址
 	    System.out.println("Loading page now-----------------------------------------------: "+url);  
 	      
 	    // HtmlUnit 模拟浏览器
@@ -50,25 +50,44 @@ public class Snippet {
 	    webClient.getOptions().setTimeout(5 * 1000);                   	//设置连接超时时间  
 	    
 	    HtmlPage page = webClient.getPage(url); 
-	    webClient.waitForBackgroundJavaScript(5 * 1000);               // 等待js后台执行秒  
+	    webClient.waitForBackgroundJavaScript(5 * 1000);               // 執行程式之後等待js運作去抓取網站的時間  
 	    String pageAsXml = page.asXml();
 	     
 	    // Jsoup解析处理  
 	    Document doc = Jsoup.parse(pageAsXml, url);    
-	    Elements allJsLinks = doc.select("a[href^='https'][class*='img_div news_list_img']"); 	//AND
+	    Elements allJsLinks =doc.select("a[href^='https'][class*='img_div news_list_img']"); 	//AND 取得標題與分頁連結
+	    Elements pcontext =doc.select("p[class*='phone_hidden']"); //這是取得P內容
+	    Elements datatext =doc.select("div[class*='fb_search_btn']"); //這是取得新聞日期內容
 //	    Elements allJsLinks = doc.select("a[href^='https'],[class*='img_div news_list_img']");	//OR
 	    // 此处省略其他操作
 	    
+	    //首頁資訊
 	    for (int i=0;i<allJsLinks.size();i++) {
 	    	String titleValue = allJsLinks.get(i).attr("title").toString();		//URL
-	    	String urlNews = allJsLinks.get(i).attr("abs:href").toString();		//URI
-	    	System.out.println(titleValue);
-	    	System.out.println(urlNews);
+	    	String urlNews = allJsLinks.get(i).attr("abs:href").toString();	//URI	
+	    	String pstr = pcontext.get(i).text();
+	    	String datastr = datatext.get(i).text();	
 	    	
-////	    	content    	
-//	    	HtmlPage pageX = webClient.getPage(url);
-//	    	HtmlDivision div = pageX.getHtmlElementById("content");
-//	    	String content = div.asText();
+	    	System.out.println(titleValue);//標題
+	    	System.out.println(urlNews);//連結
+	    	System.out.println(pstr);//內容
+	        System.out.println(datastr);//日期
+	        
+	    }
+	    
+	    /*text(),html(),attr()
+	           這幾個是比較常用的,很實用的屬性,首先text是獲取該物件的文字資訊,
+	    html是獲取該物件下面所有的html資訊(包括文字,有時候有奇效,例如table
+	          我可以將table直接顯示到自己的網頁,還不需要自己寫樣式 )
+        attr()是獲取改物件標籤中某屬性的值*/
+
+ 
+	  
+	    	
+    //	content    	
+	//HtmlPage pageX = webClient.getPage(url);
+   // HtmlDivision div = pageX.getHtmlElementById("content");
+   // String content = div.asText();
 //	    	
 ////	    	image
 //	    	
@@ -96,7 +115,7 @@ public class Snippet {
 ////			doc = Jsoup.parse(pageAsXmlX, url); 
 //////			Element jsLinks = doc.getElementsByTag("table").first();
 //			System.out.println(content);
-	    }  
+	    
 
 	    webClient.close();
 	} 
