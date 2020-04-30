e842770f354fdc8e65e4479c12920b3ebb7b60fa
ChuangRuby <glwl40039@gmail.com>
2020-04-30 21:02:32 +0800

    可以把資料輸出成txt檔,圖片尚在研究如何下載中

--- src/main/java/test/java/Snippet.java
+++ src/main/java/test/java/Snippet.java
@@ -1,16 +1,22 @@
 package test.java;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.logging.Level;
 
@@ -38,7 +44,7 @@ public class Snippet {
 	    java.util.logging.Logger.getLogger("org.apache.http.client").setLevel(Level.OFF);  
 	
 	    String url = "https://www.cardu.com.tw/message/list.php?mt_pk=2";  //要抓取的網址
-	    System.out.println("Loading page now-----------------------------------------------: "+url);  
+	    System.out.println("開始抓優惠資訊");  
 	      
 	    // HtmlUnit 模拟浏览器
 	    WebClient webClient = new WebClient(BrowserVersion.CHROME);  
@@ -57,62 +63,100 @@ public class Snippet {
 	    Document doc = Jsoup.parse(pageAsXml);    
 	    Elements allJsLinks =doc.select("a[href^='https'][class*='img_div news_list_img']"); 	//AND 取得標題與分頁連結
 	    Elements pcontext =doc.select("p[class*='phone_hidden']"); //這是取得P內容
-	    Elements datatext =doc.select("div[class*='fb_search_btn']"); //這是取得新聞日期內容
+	    Elements datatext =doc.select("span[class*='mb-1']"); //這是取得新聞日期內容
 //	    Elements allJsLinks = doc.select("a[href^='https'],[class*='img_div news_list_img']");	//OR
-	    // 此处省略其他操作
-	    
+	    Date date = new Date();//系統現在時間
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH");  //現在時間格式
+        String dateNowStr = sdf.format(date);  //格式化現在時間
+      
+        File dir_file = new File("c:/information/pageitem"); //直接建立母資料夾與子資料夾
+        if (dir_file.mkdirs()) 
+        {
+          System.out.println("已建立資料夾");
+        } else 
+        {
+         System.out.println("已存在無法再建立資料夾");
+        }
+      
+        File dir_file2 = new File("c:/information/page");   /*路徑跟檔名*/
+        if (dir_file2.mkdir()) 
+        {
+          System.out.println("已建立資料夾");
+        } else 
+        {
+         System.out.println("已存在無法再建立資料夾");
+        }
+        
+        System.out.println("共"+allJsLinks.size()+"資料會輸出");
+        
 	    //首頁資訊
-	    for (int i=0;i<allJsLinks.size();i++) {
+	   for (int i=0;i<allJsLinks.size();i++) {
 	    	String titleValue = allJsLinks.get(i).attr("title").toString();		//URL
 	    	String urlNews = allJsLinks.get(i).attr("abs:href").toString();		//URI	
 	    	String pstr = pcontext.get(i).text();
-	    	String datastr = datatext.get(i).text();	
+	    	String datastr = datatext.get(i).text();
+	    
+	        FileWriter fw1=new FileWriter("c:/information/pageitem/日期為"+dateNowStr+"首頁優惠資訊.txt",true);//產生的路徑放置位置,true為不覆蓋檔案
+	    	BufferedWriter bufferOut = new BufferedWriter(fw1,20);//20為一個緩衝區的字元可以自己設定
+	       
+	    	bufferOut.write(titleValue);//新聞標題
+	    	bufferOut.newLine();//換行
+	    	bufferOut.write(datastr);//新聞日期
+	    	bufferOut.newLine();//換行
+	    	bufferOut.write(pstr);//新聞內容
+	    	bufferOut.newLine();//換行
+	    	bufferOut.write(urlNews);//新聞連結
+	    	bufferOut.newLine();//換行
+	    	bufferOut.flush();//緩衝區內容立刻寫入檔案
+	    	fw1.close();//關檔
+	    	System.out.println("第"+(i+1)+"份首頁資料產生");
 	    	
-	    	System.out.println(titleValue);//標題
-	    	System.out.println(urlNews);//連結網站
-	    	System.out.println(pstr);//內容
-	        System.out.println(datastr);//日期
-	        
-	        
-	        
-	        page = webClient.getPage(urlNews);//從連結網站再往裡面找一次要的資料
+	    	
+	    	
+	    	//從連結網站再往裡面找一次要的資料
+	        page = webClient.getPage(urlNews);
 	        webClient.waitForBackgroundJavaScript(5 * 1000);
 	        pageAsXml = page.asXml();
 	        Document docNews = Jsoup.parse(pageAsXml);
 	        Elements alltext =docNews.select("[class*='detail_title']").select("h2"); //文章標題
 	        Elements acttime =docNews.select("[class*='detail_title']").select("p");//活動時間
-	        Elements contextarea =docNews.select("[class*='detail_content']").select("p");//活動時間
+	        Elements contextarea =docNews.select("[class*='detail_content']").select("p");//文章內容  
 	        Elements imgarea =docNews.select("[class*='con_img']").select("img");//因為要找圖片所以要爬兩層
 	      
-	        
-	        /*text(),html(),attr()這幾個是比較常用的,很實用的屬性,首先text是獲取該物件的文字資訊,
-	         html是獲取該物件下面所有的html資訊(包括文字,有時候有奇效,例如table
-	                       我可以將table直接顯示到自己的網頁,還不需要自己寫樣式 .attr()是獲取改物件標籤中某屬性的值*/
-	  
-	        
-	        String textvalue=alltext.text();//h2裡面的文字
+	    
+	        String textvalue=alltext.text();//標題
 	        String acttext=acttime.text();//活動時間
 	        String contextvalue=contextarea.get(3).text();//文章內容
 	        String imgpicture = imgarea.attr("src").toString();//找出img src連結	
-	        System.out.println("分頁開始");
-	        System.out.println(textvalue);//文章標題
-	        System.out.println(acttext);//活動時間
-	        System.out.println(contextvalue);//文章內容
-	        System.out.println(imgpicture);//圖片網址
-	        System.out.println("分頁結束");
-	        
 	         
+		    FileWriter fw2=new FileWriter("c:/information/page/日期為"+dateNowStr+"分頁優惠資訊.txt",true);//產生的路徑放置位置,true為不覆蓋檔案
+		    BufferedWriter bufferOut2 = new BufferedWriter(fw2,20);//20為一個緩衝區的字元可以自己設定
+		    bufferOut2.write(textvalue);//標題
+		    bufferOut2.newLine();//換行
+		    bufferOut2.write(acttext);//活動時間
+		    bufferOut2.newLine();//換行
+		    bufferOut2.write(contextvalue);//文章內容
+		    bufferOut2.newLine();//換行
+		    bufferOut2.flush();//緩衝區內容立刻寫入檔案
+            fw2.close();
+            System.out.println("第"+(i+1)+"份分資料產生"); 
+	         
+	       
+	    
 	        
-	    }
+	        }
+	        System.out.println("全部資料皆以輸出"); 
 	    
-	
-
- 
-	  
-	
 	    
-
+	    
+	    
 	    webClient.close();
 	} 
+	
+	
+	
 }
+
+
+
 
