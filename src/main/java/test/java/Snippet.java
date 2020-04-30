880463b85a2c47b3a76c2459f74e120094c268ab
ChuangRuby <glwl40039@gmail.com>
2020-04-30 10:47:11 +0800

    可以抓到首頁與分頁的資料了~但還不能把資料抓到本地..

--- src/main/java/test/java/Snippet.java
+++ src/main/java/test/java/Snippet.java
@@ -37,7 +37,7 @@ public class Snippet {
 	    // 屏蔽HtmlUnit等系统 log  
 	    java.util.logging.Logger.getLogger("org.apache.http.client").setLevel(Level.OFF);  
 	
-	    String url = "https://www.cardu.com.tw/news/list.php?nt_pk=6";  //要抓取的網址
+	    String url = "https://www.cardu.com.tw/message/list.php?mt_pk=2";  //要抓取的網址
 	    System.out.println("Loading page now-----------------------------------------------: "+url);  
 	      
 	    // HtmlUnit 模拟浏览器
@@ -54,7 +54,7 @@ public class Snippet {
 	    String pageAsXml = page.asXml();
 	     
 	    // Jsoup解析处理  
-	    Document doc = Jsoup.parse(pageAsXml, url);    
+	    Document doc = Jsoup.parse(pageAsXml);    
 	    Elements allJsLinks =doc.select("a[href^='https'][class*='img_div news_list_img']"); 	//AND 取得標題與分頁連結
 	    Elements pcontext =doc.select("p[class*='phone_hidden']"); //這是取得P內容
 	    Elements datatext =doc.select("div[class*='fb_search_btn']"); //這是取得新聞日期內容
@@ -64,57 +64,52 @@ public class Snippet {
 	    //首頁資訊
 	    for (int i=0;i<allJsLinks.size();i++) {
 	    	String titleValue = allJsLinks.get(i).attr("title").toString();		//URL
-	    	String urlNews = allJsLinks.get(i).attr("abs:href").toString();	//URI	
+	    	String urlNews = allJsLinks.get(i).attr("abs:href").toString();		//URI	
 	    	String pstr = pcontext.get(i).text();
 	    	String datastr = datatext.get(i).text();	
 	    	
 	    	System.out.println(titleValue);//標題
-	    	System.out.println(urlNews);//連結
+	    	System.out.println(urlNews);//連結網站
 	    	System.out.println(pstr);//內容
 	        System.out.println(datastr);//日期
 	        
+	        
+	        
+	        page = webClient.getPage(urlNews);//從連結網站再往裡面找一次要的資料
+	        webClient.waitForBackgroundJavaScript(5 * 1000);
+	        pageAsXml = page.asXml();
+	        Document docNews = Jsoup.parse(pageAsXml);
+	        Elements alltext =docNews.select("[class*='detail_title']").select("h2"); //文章標題
+	        Elements acttime =docNews.select("[class*='detail_title']").select("p");//活動時間
+	        Elements contextarea =docNews.select("[class*='detail_content']").select("p");//活動時間
+	        Elements imgarea =docNews.select("[class*='con_img']").select("img");//因為要找圖片所以要爬兩層
+	      
+	        
+	        /*text(),html(),attr()這幾個是比較常用的,很實用的屬性,首先text是獲取該物件的文字資訊,
+	         html是獲取該物件下面所有的html資訊(包括文字,有時候有奇效,例如table
+	                       我可以將table直接顯示到自己的網頁,還不需要自己寫樣式 .attr()是獲取改物件標籤中某屬性的值*/
+	  
+	        
+	        String textvalue=alltext.text();//h2裡面的文字
+	        String acttext=acttime.text();//活動時間
+	        String contextvalue=contextarea.get(3).text();//文章內容
+	        String imgpicture = imgarea.attr("src").toString();//找出img src連結	
+	        System.out.println("分頁開始");
+	        System.out.println(textvalue);//文章標題
+	        System.out.println(acttext);//活動時間
+	        System.out.println(contextvalue);//文章內容
+	        System.out.println(imgpicture);//圖片網址
+	        System.out.println("分頁結束");
+	        
+	         
+	        
 	    }
 	    
-	    /*text(),html(),attr()
-	           這幾個是比較常用的,很實用的屬性,首先text是獲取該物件的文字資訊,
-	    html是獲取該物件下面所有的html資訊(包括文字,有時候有奇效,例如table
-	          我可以將table直接顯示到自己的網頁,還不需要自己寫樣式 )
-        attr()是獲取改物件標籤中某屬性的值*/
+	
 
  
 	  
-	    	
-    //	content    	
-	//HtmlPage pageX = webClient.getPage(url);
-   // HtmlDivision div = pageX.getHtmlElementById("content");
-   // String content = div.asText();
-//	    	
-////	    	image
-//	    	
-//	    	String img = div.getElementsByTagName("img").toString().substring(28,90); 
-//
-//	    	System.out.println(img);
-//	    	
-//            String imageName = img.substring(56);
-//            URL urlImg = new URL(img);
-//            URLConnection uri = urlImg.openConnection();
-//            InputStream is = uri.getInputStream();
-//            OutputStream os = new FileOutputStream(new File("data/images/news/"+imageName+".jpg"));
-//
-//            byte[] buf = new byte[1024];
-//            int len = -1;
-//
-//            while ((len = is.read(buf)) != -1) {
-//                os.write(buf, 0, len);
-//            }
-//	    	
-//	    	
-//	    	String pageAsXmlT = pageX.asText();
-//	    	String pageAsXmlX = pageX.asXml();
-//	    	doc = Jsoup.parse(pageAsXmlX, url);
-////			doc = Jsoup.parse(pageAsXmlX, url); 
-//////			Element jsLinks = doc.getElementsByTag("table").first();
-//			System.out.println(content);
+	
 	    
 
 	    webClient.close();
